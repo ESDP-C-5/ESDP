78c1eee84658bd3e41355c92dee49e52cbe22d69
dzhunushev3 <dzhunushev3@gmail.com>
2019-11-07 15:42:10 +0600

    Init

---
+++ Repositories/BaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using CRM.Data;
+using CRM.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Repositories
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly ApplicationDbContext _context;
+        protected DbSet<T> DbSet;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            DbSet = _context.Set<T>();
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await DbSet.AddAsync(entity);
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            return DbSet.ToListAsync();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            return DbSet.FindAsync(id);
+        }
+
+        public void RemoveAsync(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public void UpdateAsync(T entity)
+        {
+            DbSet.Update(entity);
+        }
+    }
+}
---
+++ UoW/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using CRM.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.UoW
+{
+    public class UnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public Task<int> CompleteAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
