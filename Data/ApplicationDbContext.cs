df3b1fcd3e84114509dda558748ca51ff1405de4
Mike <coolup@mail.ru>
2019-11-07 20:37:55 +0600

    AddModels_GroupeandLevel

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,11 +4,13 @@ using System.Text;
 using CRM.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 
 namespace CRM.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+       
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ Models/Groupe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CRM.Models
+{
+    public class Groupe : BaseEntity
+    {
+        private ApplicationUser User { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string UserId { get; set; }
+        public  int BranchId { get; set; }
+        public int LevelId { get; set; }
+    }
+}
