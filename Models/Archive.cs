6c96b3b4cc62fd35b4b56b17e1d0ba61791f5297
tokurov <tokurof29@gmail.com>
2019-11-08 19:03:38 +0600

    Archive and Time Table

---
+++ Models/Archive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Models
+{
+    public class Archive : BaseEntity
+    {
+        public int UserId { get; set; }
+        public DateTime DataStart { get; set; }
+        public int GroupeId { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Models/TimeTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Models
+{
+    public class TimeTable : BaseEntity
+    {
+        public DateTime DateStarT { get; set; }
+        public DateTime DateEnd { get; set; }
+    }
+}
