160c287e0d4e0e46b5b9cb7b4ce0e0d10d592b26
tokurov <tokurof29@gmail.com>
2019-11-12 19:39:44 +0600

    Created Repositories for Branch and Level

---
+++ Repositories/BranchRepository.cs
@@ -0,0 +1,16 @@
+﻿using CRM.Data;
+using CRM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Repositories
+{
+    public class BranchRepository : BaseRepository<Branch>
+    {
+        public BranchRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
