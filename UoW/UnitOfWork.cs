cd1b697fd8c6ed2c445c1faafd9366830be2fa7b
tokurov <tokurof29@gmail.com>
2019-11-12 19:49:40 +0600

    Added BranchRepository and LevelRepository to Unit of Work

--- UoW/UnitOfWork.cs
+++ UoW/UnitOfWork.cs
@@ -12,11 +12,15 @@ namespace CRM.UoW
         private readonly ApplicationDbContext _context;
         public GroupRepository Groups;
         public StudentRepository Student;
+        public BranchRepository Branchs;
+        public LevelRepository Levels;
         public UnitOfWork(ApplicationDbContext context)
         {
             _context = context;
             Groups = new GroupRepository(context);
             Student = new StudentRepository(context);
+            Branchs = new BranchRepository(context);
+            Levels = new LevelRepository(context);
 
         }
 
