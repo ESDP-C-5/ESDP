d74a71734be50f470ee6e63b0429a5aeaf3ef559
tokurov <tokurof29@gmail.com>
2019-11-22 19:27:48 +0600

    #60 Validation on Branch creation
    Added requirements with danger text in russian

--- Models/Branch.cs
+++ Models/Branch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -8,8 +9,9 @@ namespace CRM.Models
 {
     public class Branch : BaseEntity
     {
+        [Required(ErrorMessage = "Данное поле не может быть пустым")]
         public string Name { get; set; }
-
+        [Required(ErrorMessage = "Поле адреса не может быть пустым")]
         public string Address { get; set; }
     }
 }
