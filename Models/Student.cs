96f4eaead2debc9bed21027e9cae35a5980c4bbb
Ravshan89 <ravshanbaltabaev@gmail.com>
2019-11-22 19:41:01 +0600

    #59 Validation on Student creting

--- Models/Student.cs
+++ Models/Student.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,20 +10,30 @@ namespace CRM.Models
 {
     public class Student : BaseEntity
     {
+        [Required(ErrorMessage = "Не указано имя")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Длина имени должна быть от 2 до 50 символов")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Не указана фамилия")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Длина фамилии должна быть от 2 до 50 символов")]
         public string LastName { get; set; }
         public string FatherName { get; set; }
+        [Required]
         public int? LevelId { get; set; }
+        [Required]
         public DateTime DateOfBirthday { get; set; }
         public DateTime TrialDate { get; set; }
+        [Required(ErrorMessage = "Не указано имя")]
         public string ParentName { get; set; }
+        [Required(ErrorMessage = "Не указана фамилия")]
         public string ParentLastName { get; set; }
         public string ParentFatherName { get; set; }
+        [Required(ErrorMessage = "Укажите номер телефона")]
         public string PhoneNumber { get; set; }
         public string Comment { get; set; }
+        [Required]
         public int? GroupId { get; set; }
- 
-        public StudentStatusEnum Status { get; set; }  
+        public StudentStatusEnum Status { get; set; }
+        [Required]
         public Level Level { get; set; }
         public Group Group { get; set; }
 
