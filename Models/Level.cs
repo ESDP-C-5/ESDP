a7cf38890c4ca4a947db865cd5e0d01abba57d01
tokurov <tokurof29@gmail.com>
2019-11-26 19:27:54 +0600

    #61 Adding validation requirements to Level model
    To the field Name added requirements: 1. field Name can't be empty
    2. Length of Name can't be shorter than 2 characters and can't be longer than 50 characters

--- Models/Level.cs
+++ Models/Level.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace CRM.Models
 {
     public class Level : BaseEntity
     {
+        [Required(ErrorMessage = "Названия уровня не может быть пустым")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Длина не должна быть от 2 до 50 символов")]
         public string Name { get; set; }
     }
 }
