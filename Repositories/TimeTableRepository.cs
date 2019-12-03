d0a433109d05dfb63fe4bea6d6203541505b7e13
dzhunushev3 <32898045+dzhunushev3@users.noreply.github.com>
2019-12-03 18:16:40 +0600

    #87 Create TimeTableController  Create views for TimeTableController
    add validation for TimeTable model

--- Repositories/TimeTableRepository.cs
+++ Repositories/TimeTableRepository.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using CRM.Data;
+using CRM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CRM.Repositories
 {
-    public class TimeTableRepository
+    public class TimeTableRepository : BaseRepository<TimeTable>
     {
+        public TimeTableRepository(ApplicationDbContext context) : base(context)
+        {
+        }
     }
 }
