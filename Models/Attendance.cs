bba07dabecdf9cba63e4a8def10e8fea151201b2
tokurov <tokurof29@gmail.com>
2019-12-05 17:04:43 +0600

    #81 Adding class Attendance
    Created class Attendance with fields:
    StudebtID
    Day (specific day)
    boolean IsAttended
    Comment

---
+++ Models/Attendance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Models
+{
+    public class Attendance
+    {
+        public int StudentId { get; set; }
+        public DateTime Day { get; set; }
+        public bool IsAttended { get; set; }
+
+        [StringLength(300)]
+        public string Comment { get; set; }
+    }
+}
