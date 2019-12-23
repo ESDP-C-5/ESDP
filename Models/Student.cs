8c57172bad156e0a4b36766fa708037f221a62a1
Ravshan89 <ravshanbaltabaev@gmail.com>
2019-12-23 19:26:45 +0600

    #145_EditedModelStudent Remove Required from Student Fields Add Migration EditedModelStudent

--- Models/Student.cs
+++ Models/Student.cs
@@ -10,27 +10,19 @@ namespace CRM.Models
 {
     public class Student : BaseEntity
     {
-        [Required(ErrorMessage = "Не указано имя")]
-        [StringLength(50, MinimumLength = 2, ErrorMessage = "Длина имени должна быть от 2 до 50 символов")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Не указана фамилия")]
-        [StringLength(50, MinimumLength = 2, ErrorMessage = "Длина фамилии должна быть от 2 до 50 символов")]
         public string LastName { get; set; }
         public string FatherName { get; set; }
         public int? LevelId { get; set; }
-        [Required]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}", ApplyFormatInEditMode = true)]
         [Display(Name = "Дата рождения")]
         public DateTime DateOfBirthday { get; set; }
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}", ApplyFormatInEditMode = true)]
         public DateTime TrialDate { get; set; }
+        [DataType(DataType.Date)]
         public DateTime CreatedDate { get; set; }
+        [DataType(DataType.Date)]
         public DateTime ChangeStatusDate { get; set; }
-        [Required(ErrorMessage = "Не указано имя")]
         public string ParentName { get; set; }
-        [Required(ErrorMessage = "Не указана фамилия")]
         public string ParentLastName { get; set; }
         public string ParentFatherName { get; set; }
         [Required(ErrorMessage = "Укажите номер телефона")]
