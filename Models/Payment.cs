cd59706b7ffa611be1caf72bfb37653de8d44f1d
dzhunushev3 <32898045+dzhunushev3@users.noreply.github.com>
2020-01-15 13:56:53 +0600

    #160 create Model Payment and StudentPaymentAndPeriod, change model Student

---
+++ Models/Payment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRM.Models
+{
+    public class Payment : BaseEntity
+    {
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public StudentPaymentAndPeriod StudentPaymentAndPeriod { get; set; }
+        public int StudentPaymentAndPeriodId { get; set; }
+        public decimal Total { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateTimePayment { get; set; }
+        public string Comment { get; set; }
+        public Payment()
+        {
+            DateTimePayment = DateTime.Now;
+        }
+    }
+}
